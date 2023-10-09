360b4393a77652194169845f73337217b0421ae6
Rincko <guilhermericardopro71@gmail.com>
2023-10-08 23:16:04 -0400

    discord events quick fix

--- packages/discord-events/src/events/guildMemberVoiceChannelMove.ts
+++ packages/discord-events/src/events/guildMemberVoiceChannelMove.ts
@@ -1,7 +1,9 @@
-import { AuditLogEvent, Client, VoiceState } from "discord.js";
+import { AuditLogEvent, ChannelType, Client, VoiceState } from "discord.js";
 
 export async function guildMemberVoiceChannelMove(client: Client, oldSate: VoiceState, newState: VoiceState){
-    if (!oldSate.channel || !newState.channel) return;
+    if (oldSate.channel?.type !== ChannelType.GuildVoice) return;
+    if (newState.channel?.type !== ChannelType.GuildVoice) return;
+    if (!newState.member) return;
     const { entries } = await newState.guild.fetchAuditLogs({
         type: AuditLogEvent.MemberMove,
         limit: 1,
@@ -9,9 +11,12 @@ export async function guildMemberVoiceChannelMove(client: Client, oldSate: Voice
     const entry = entries.first();
     if (!entries.size || !entry) return;
     if (!entry.executor) return;
+    const member = newState.guild.members.cache.get(entry.executor.id);
+    if (!member) return;
+
 
     client.emit("guildMemberVoiceChannelMove", 
         newState.member, newState.channel,
-        entry.executor, oldSate.channel
+        member, oldSate.channel
     )
 }
