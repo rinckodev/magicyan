c11201b9625ac7cde8e0c99bfbdb31bd473ca36b
Rincko <guilhermericardopro71@gmail.com>
2023-12-20 00:07:05 -0400

    quick fix

--- packages/core/src/functions/promises.ts
+++ packages/core/src/functions/promises.ts
@@ -10,17 +10,3 @@ export function createInterval({ time, run }: CreateIntervalOptions){
     }, time);
     return { timer, stop: () => clearInterval(timer) };
 }
-
-interface CreateTimeoutOptions {
-    time: number,
-    run(repeat: () => void): void
-}
-export function createTimeout({ time, run }: CreateTimeoutOptions){
-    const timer = setTimeout(function(){
-        
-    }, time)
-    // const timer = setTimeout(() => {
-    //     run(() => clearInterval(timer));
-    // }, time);
-    // return { timer, stop: () => clearInterval(timer) };
-}
