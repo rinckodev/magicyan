864d101c31187600208d5ff8547c029e0a849463
Rincko <guilhermericardopro71@gmail.com>
2024-01-10 19:50:17 -0400

    lib utils

--- build.preset.ts
+++ build.preset.ts
@@ -3,12 +3,11 @@ import { definePreset } from "unbuild";
 export default definePreset({
 	clean: true,
 	declaration: true,
-	sourcemap: true,
 	rollup: {
 		emitCJS: true,
-		inlineDependencies: true,
-		esbuild: {
-			minify: true,
-		},
+		output: {
+			preserveModules: true,
+			preserveModulesRoot: "./src"
+		}
 	},
 });
