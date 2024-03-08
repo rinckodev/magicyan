f945c454874f3fb8887f815011f7521c9d2b597d
Rincko <guilhermericardopro71@gmail.com>
2024-03-08 08:05:28 -0400

    quick fix - discord events

--- packages/discord-events/src/events/messages/webhookMessageCreate.ts
+++ packages/discord-events/src/events/messages/webhookMessageCreate.ts
@@ -3,6 +3,7 @@ import type { Message } from "discord.js";
 export async function webhookMessageCreate(message: Message){
     if (!message.inGuild() || !message.webhookId) return;
     
-    const webhook = await message.fetchWebhook();
+    const webhook = await message.fetchWebhook().catch(() => null);
+    if (!webhook) return;
     message.client.emit("webhookMessageCreate", message, webhook);
 }
